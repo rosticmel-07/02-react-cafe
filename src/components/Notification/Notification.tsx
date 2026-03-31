db60a4fa8e94b7843297f2c049f7bc367a17c196
Rostik <rosticmel@gmail.com>
2026-03-31 17:23:13 +0300

    fix ; in Notification

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -2,7 +2,7 @@ import css from "./Notification.module.css";
 export default function Notification() {
   return (
     <>
-      <p className={css.message}>No feedback yet</p>;
+      <p className={css.message}>No feedback yet</p>
     </>
   );
 }
